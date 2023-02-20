7f4cbefe3c53adada225a03f86edbe8c6a39a2f2
kazuma-0 <hello@kazuma.in>
2023-02-20 10:50:50 +0530

    update

--- src/components/nav/people.tsx
+++ src/components/nav/people.tsx
@@ -1,28 +1,31 @@
-import { Box, Flex, Heading, Link } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function People() {
-  return (
-    <Box>
-      <Heading pb={2} size={"md"}>
-        People
-      </Heading>
-      <Flex gap={2} flexDir={"column"}>
-        <Box as={Link} href="/people/professor">
-          Professor
-        </Box>
-        <Box as={Link} href="/people/current-students">
-          Current Students
-        </Box>
-        <Box as={Link} href="/people/post-doctoral-researchers">
-          Post doctoral Researchers
-        </Box>
-        <Box as={Link} href="/people/interns">
-          Interns
-        </Box>
-        <Box as={Link} href="/people/alumni">
-          Alumni
-        </Box>
-      </Flex>
-    </Box>
-  );
+	const router = useRouter();
+	return (
+		<Box>
+			<Heading pb={2} size={"md"}>
+				People
+			</Heading>
+			<Flex gap={2} flexDir={"column"}>
+				<Box as={Link} href='/people/professor'>
+					Professor
+				</Box>
+				<Box as={Link} href='/people/current-students'>
+					Current Students
+				</Box>
+				<Box as={Link} href='/people/post-doctoral-researchers'>
+					Post doctoral Researchers
+				</Box>
+				<Box as={Link} href='/people/interns'>
+					Interns
+				</Box>
+				<Box as={Link} href='/people/alumni'>
+					Alumni
+				</Box>
+			</Flex>
+		</Box>
+	);
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,97 +1,113 @@
 import {
-  Box,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  Fade,
-  Flex,
-  Grid,
-  Heading,
-  Input,
-  ListItem,
-  Portal,
-  UnorderedList,
-  useDisclosure,
+	Box,
+	Button,
+	Drawer,
+	DrawerBody,
+	DrawerCloseButton,
+	DrawerContent,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay,
+	Fade,
+	Flex,
+	Grid,
+	Heading,
+	Input,
+	ListItem,
+	Portal,
+	UnorderedList,
+	useDisclosure,
 } from "@chakra-ui/react";
 import axios from "axios";
 import Link from "next/link";
 import { HiMenu, HiOutlineMenuAlt2 } from "react-icons/hi";
 import { useMutation, useQuery } from "react-query";
 import { People, Publications, Research } from "@/components/nav";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
 export function NavBar() {
-  const { data, isFetching } = useQuery({
-    queryKey: ["fetch-publications-research"],
-    queryFn: async () => {
-      const { data } = await axios.get("/api/projects-and-research");
-      return data;
-    },
-  });
+	const router = useRouter();
+	const { data, isFetching } = useQuery({
+		queryKey: ["fetch-publications-research"],
+		queryFn: async () => {
+			const { data } = await axios.get("/api/projects-and-research");
+			return data;
+		},
+	});
+	const prevRoute = useRef("");
+	const { isOpen, onClose, onOpen } = useDisclosure({});
+	useEffect(() => {
+		onClose();
+		console.log("Path changed", router.pathname);
+	}, [router.pathname]);
+	return (
+		<>
+			<Flex
+				px={[3, 10]}
+				alignItems='center'
+				h={16}
+				borderBottom='black 1px solid'
+				justifyContent={"space-between"}
+			>
+				<Box>
+					<Heading size='lg'>
+						Center for Resilient and Evolving Intelligence (CREI)
+					</Heading>
+				</Box>
+				<Box cursor='pointer'>
+					<HiMenu onClick={onOpen} size={32} />
+				</Box>
+			</Flex>
+			<Portal>
+				<Drawer size='xl' onClose={onClose} isOpen={isOpen}>
+					<DrawerOverlay></DrawerOverlay>
+					<DrawerContent>
+						<DrawerCloseButton />
+						<DrawerBody>
+							<Box>
+								<Input
+									maxW='container.sm'
+									variant={"flushed"}
+									size='lg'
+									placeholder='Search...'
+								></Input>
+							</Box>
 
-  const { isOpen, onClose, onOpen } = useDisclosure({});
-  return (
-    <>
-      <Flex
-        px={[3, 10]}
-        alignItems="center"
-        h={16}
-        borderBottom="black 1px solid"
-        justifyContent={"space-between"}
-      >
-        <Box>
-          <Heading size="lg">
-            Center for Resilient and Evolving Intelligence (CREI)
-          </Heading>
-        </Box>
-        <Box cursor="pointer">
-          <HiMenu onClick={onOpen} size={32} />
-        </Box>
-      </Flex>
-      <Portal>
-        <Drawer size="xl" onClose={onClose} isOpen={isOpen}>
-          <DrawerOverlay></DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerBody>
-              <Box>
-                <Input
-                  maxW="container.sm"
-                  variant={"flushed"}
-                  size="lg"
-                  placeholder="Search..."
-                ></Input>
-              </Box>
-
-              <Grid
-                py={5}
-                gap={10}
-                gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]}
-              >
-                <Research research_publications={data?.research_publications} />
-                <People />
-                <Publications publications={data?.publications}></Publications>
-                {/* GALLERY */}
-                <Box>
-                  <Heading size={"md"}>Gallery</Heading>
-                </Box>
-                {/* CONTACT US */}
-                <Box>
-                  <Heading size={"md"}>Contact Us</Heading>
-                </Box>
-                {/* NEWS */}
-                <Box>
-                  <Heading size={"md"}>News</Heading>
-                </Box>
-              </Grid>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-      </Portal>
-    </>
-  );
+							<Grid
+								py={5}
+								gap={10}
+								gridTemplateColumns={[
+									"repeat(1, 1fr)",
+									"repeat(2, 1fr)",
+								]}
+							>
+								<Research
+									research_publications={
+										data?.research_publications
+									}
+								/>
+								<People />
+								<Publications
+									publications={data?.publications}
+								></Publications>
+								{/* GALLERY */}
+								<Box>
+									<Heading size={"md"}>Gallery</Heading>
+								</Box>
+								{/* CONTACT US */}
+								<Box>
+									<Heading size={"md"}>Contact Us</Heading>
+								</Box>
+								{/* NEWS */}
+								<Box>
+									<Heading size={"md"}>News</Heading>
+								</Box>
+							</Grid>
+						</DrawerBody>
+					</DrawerContent>
+				</Drawer>
+			</Portal>
+		</>
+	);
 }
